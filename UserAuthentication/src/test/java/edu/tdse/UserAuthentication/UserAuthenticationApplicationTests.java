7361e32563f609382eedb2392afa098d07e26a1b
Tulio <tuliojr3101@gmail.com>
2026-04-19 19:42:39 -0500

    feat: adding microservices and removing .env from frontend

---
+++ UserAuthentication/src/test/java/edu/tdse/UserAuthentication/UserAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tdse.UserAuthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
